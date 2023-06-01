cc3b0d43302aa546c6f1ff0e2c4afaa8ab7748de
SWhalley1130 <s.whalley1130@gmail.com>
2023-06-01 09:58:14 -0600

    Restructures modules

---
+++ Databases/src/Main.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+        JOptionPane.showInputDialog("This is a test 2.");
+
+        System.exit(0);
+    }
+}
--- inheritance/src/Main.java
+++ inheritance/src/Main.java
@@ -1,6 +1,6 @@
 import javax.swing.JOptionPane;
 
-public class Main 
+public class Main
 {
     public static void main(String[] args)
     {
