c73745e8b24d8fcbd95def87515c2876802348f2
SWhalley1130 <s.whalley1130@gmail.com>
2023-06-08 17:57:01 -0600

    Demonstrates Object class inheritance

--- inheritance/src/Main.java
+++ inheritance/src/Main.java
@@ -5,25 +5,16 @@ public class Main
 {
     public static void main(String[] args)
     {
-        int questions, missed;
-        double minPassing;
+        PassFailExam exam1=new PassFailExam(0,0,0);
+        PassFailExam exam2= new PassFailExam(0,0,0);
 
-        Scanner keyboard=new Scanner(System.in);
+        System.out.println(exam1);
+        System.out.println(exam2);
 
-        System.out.print("How many questions are on the exam?: ");
-        questions=keyboard.nextInt();
-
-        System.out.print("How many questions did the student miss? ");
-        missed=keyboard.nextInt();
-
-        System.out.print("What is the minimum passing score?: ");
-        minPassing=keyboard.nextDouble();
-
-        PassFailExam exam = new PassFailExam(questions, missed, minPassing);
-
-        System.out.println("Each questions counts as "+exam.getPointsEach()+" points.");
-        System.out.println("The exam score is "+exam.getScore());
-        System.out.println("The exam grade is "+exam.getGrade());
+        if (exam1.equals(exam2))
+            System.out.println("They are the same.");
+        else
+            System.out.println("They are not the same.");
 
     }
 
